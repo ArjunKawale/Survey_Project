4b48dae82bd3c6d5281f4f103bbe9c179ef55237
ArjunKawale <arjunkawle15@gmail.com>
2025-06-27 13:03:00 +0530

    test

---
+++ api/submitForm/index.js
@@ -0,0 +1,37 @@
+const { TableClient, AzureNamedKeyCredential } = require("@azure/data-tables");
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = async function (context, req) {
+  const data = req.body;
+
+  const account = process.env.STORAGE_ACCOUNT_NAME;
+  const accountKey = process.env.STORAGE_ACCOUNT_KEY;
+  const tableName = "SurveyResponses";
+
+  const credential = new AzureNamedKeyCredential(account, accountKey);
+  const client = new TableClient(`https://${account}.table.core.windows.net`, tableName, credential);
+
+  const entity = {
+    partitionKey: "survey",
+    rowKey: uuidv4(),
+    name: data.name,
+    email: data.email,
+    field: data.field,
+    politics: data.politics,
+    globalEvent: data.globalEvent,
+    sciFi: data.sciFi
+  };
+
+  try {
+    await client.createEntity(entity);
+    context.res = {
+      status: 200,
+      body: "Survey submitted!"
+    };
+  } catch (err) {
+    context.res = {
+      status: 500,
+      body: "Error: " + err.message
+    };
+  }
+};
